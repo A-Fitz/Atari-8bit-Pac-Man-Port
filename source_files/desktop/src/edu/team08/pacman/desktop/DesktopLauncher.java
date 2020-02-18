d82c05fb444bc8c4bc95a08666880ca3418dc012
Stel312 <nstelken312@gmail.com>
2020-02-17 22:56:26 -0600

    intial commit SEMS20T08-1

---
+++ source_files/desktop/src/edu/team08/pacman/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package edu.team08.pacman.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import edu.team08.pacman.PacManGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PacManGame(), config);
+	}
+}
