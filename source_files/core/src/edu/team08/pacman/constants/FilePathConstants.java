f943148274af2f78d4bf96b229a19cc02d4f134f
Austin FitzGerald <fitzgerald.austind@gmail.com>
2020-03-14 19:22:06 -0500

    Added system independent filepath seperator.

--- source_files/core/src/edu/team08/pacman/constants/FilePathConstants.java
+++ source_files/core/src/edu/team08/pacman/constants/FilePathConstants.java
@@ -1,7 +1,9 @@
 package edu.team08.pacman.constants;
 
+import java.io.File;
+
 public class FilePathConstants
 {
-    public static final String TILEDMAP_PATH = "map/map.tmx";
-    public static final String SPRITES_PATH = "sprites/actors.atlas";
+    public static final String TILEDMAP_PATH = "map" + File.separator + "map.tmx";
+    public static final String SPRITES_PATH = "sprites" + File.separator + "actors.atlas";
 }
